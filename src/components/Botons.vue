562a5a256d56a207cdf58547bcb85dcfbbe46b2c
XAXAXkw <98657612+XAXAXkw@users.noreply.github.com>
2022-11-14 17:32:37 +0100

    Add files via upload

---
+++ src/components/Botons.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button @click="$emit('btnEvent')">
+      Array INDEX: {{ currentSentence }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Botons",
+  props: { currentSentence: { type: Number, required: true } },
+};
+</script>
+
+<style lang="scss" scoped></style>
